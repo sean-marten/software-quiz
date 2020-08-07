67152bdeef523cdfbb7bef72e422c83dae3ff0c2
smarten <smarten.developer@gmail.com>
2020-08-06 21:09:12 -0700

    logic to determine correct and button to continue to next question

--- assets/logic.js
+++ assets/logic.js
@@ -2,25 +2,33 @@ $(document).ready(function () {
     let $response = $(".response");
   let $container = $(".container");
   const $button = $(".btn");
-  let index = 0;
+  let qIndex = 0;
   renderQuestions(0);
   $button.attr('disabled', true);
 
-  function renderQuestions(index) {
+  function renderQuestions() {
       $container.html("");
-    const $li = $("<li>").text(questions[index].questionText);
-    $container.append($li).attr('data-index', index);
-    for (j = 0; j < questions[index].answers.length; j++) {
-      const $li = $("<li>").text(questions[index].answers[j].text).addClass('answer');
+      $response.html("");
+    const $li = $("<li>").text(questions[qIndex].questionText);
+    $container.append($li).attr('q-index', qIndex);
+    for (j = 0; j < questions[qIndex].answers.length; j++) {
+      const $li = $("<li>").text(questions[qIndex].answers[j].text).addClass('answer').attr('a-index', j);
       $container.append($li);
     }
   }
 
-  $container.on('click', '.answer', function(){
-        $response.text("you clicked")
+  $container.on('click', '.answer', function(e){
+      var ind = $(this).attr('a-index');
+      if (questions[qIndex].answers[ind].correct) {
+        $response.text("Correct!")
+      } else {
+        $response.text("Wrong.")
+      }
         $button.prop('disabled', false)
   })
 
   $button.on('click', function() {
+      qIndex++;
+      renderQuestions();
   })
 });
